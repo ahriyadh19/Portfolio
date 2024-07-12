b0a25730e647a3c9d15a7ff7e13747d01124da3f
AHMED RIYADH MOHAMMED AHMED <alahmedriyadh1993@gmail.com>
2024-07-12 22:18:42 +0300

    Update resume content and skills section

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 // script.js
 
 // Get the screen width and height
-var screenWidth = window.innerWidth;
-var screenHeight = window.innerHeight;
+const screenWidth = window.innerWidth;
+const screenHeight = window.innerHeight;
 
-// Calculate the number of spans based on screen width divided by 25
-var numberOfSpans = Math.floor(screenWidth / 40);
+// Calculate the number of spans based on screen width divided by 40
+const numberOfSpans = Math.floor(screenWidth / 40);
 
 // Generate spans with random --i values and animation properties
 for (let i = 1; i <= numberOfSpans; i++) {
-    var randomValue = Math.floor(Math.random() * 10) + 1;
-    var span = document.createElement('span');
+    const randomValue = Math.floor(Math.random() * 10) + 1;
+    const span = document.createElement('span');
+
+    // Set initial and final positions
+    const startX = Math.random() * screenWidth;
+    const startY = Math.random() * screenHeight;
+    const endX = Math.random() * screenWidth;
+    const endY = Math.random() * screenHeight;
+
+    // Set random properties
     span.style.setProperty('--i', randomValue);
-    span.style.setProperty('--delay', Math.random() * 5 + 1 + 's'); // Random delay
-    span.style.setProperty('--duration', Math.random() * 10 + 5 + 's'); // Random duration
-    span.style.setProperty('--translateX', (Math.random() * screenWidth * 2 - screenWidth) + 'px'); // Random translateX with negative and positive values
-    span.style.setProperty('--translateY', (Math.random() * screenHeight * 2 - screenHeight) + 'px'); // Random translateY with negative and positive values
-    span.style.setProperty('--scale', Math.random() * 2); // Random scale
+    span.style.setProperty('--delay', `${Math.random() * 5 + 1}s`); // Random delay
+    span.style.setProperty('--duration', `${Math.random() * 10 + 5}s`); // Random duration
+    span.style.setProperty('--translateX', `${startX}px`); // Random start translateX
+    span.style.setProperty('--translateY', `${startY}px`); // Random start translateY
+    span.style.setProperty('--finalTranslateX', `${endX}px`); // Random end translateX
+    span.style.setProperty('--finalTranslateY', `${endY}px`); // Random end translateY
+    span.style.setProperty('--scale', Math.random() * 2); // Random start scale
+    span.style.setProperty('--finalScale', Math.random() * 2); // Random end scale
+
     document.querySelector('.wrap').appendChild(span);
 }
